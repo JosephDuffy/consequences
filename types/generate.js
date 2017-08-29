3a1f3a50d54f1b380be51ea0aa6b67e357bedace
Joseph Duffy <josephduffy@me.com>
2017-08-29 20:19:59 +0100

    Adds `types/generate.js`, a script used to generate `.d.ts` files with exported modules
    Updates exported types files

---
+++ types/generate.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+function generateModule(moduleName) {
+  const outputFileLocation = `./types/${moduleName}/index.d.ts`;
+  const command = `./node_modules/.bin/dts-bundle-generator ./types/${moduleName}/index.ts -o ${outputFileLocation}`;
+
+  exec(command, (error) => {
+    if (error) {
+      throw new Error(error);
+    }
+
+    const fileContents = fs.readFileSync(outputFileLocation);
+    const fileLines = fileContents.toString().split('\n');
+    const indentedLines = fileLines.map((line) => {
+      // Remove any `declare` statements since we're now wrapping the whole file in our own `declare`
+      const cleanedUpLine = line.replace('declare ', '');
+
+      // Pad with 4 spaces to keep indentation correct
+      return `    ${cleanedUpLine}`
+    });
+    const finalLines = [
+      `declare module "consequences/${moduleName}" {`,
+      ...indentedLines,
+      '}'
+    ];
+    const finalContents = finalLines.reduce((file, line) => `${file}${line}\n`, '');
+    fs.writeFileSync(outputFileLocation, finalContents);
+  });
+}
+
+generateModule('addons');
+generateModule('api');
