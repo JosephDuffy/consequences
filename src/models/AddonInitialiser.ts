eac03b26075cf44891667efd9f81e85513f862cc
Joseph Duffy <josephduffy@me.com>
2017-09-04 21:18:04 +0100

    Improves documentation of `AddonInitialiser`

--- src/models/AddonInitialiser.ts
+++ src/models/AddonInitialiser.ts
@@ -2,10 +2,25 @@ import Addon from './Addon';
 
 interface AddonInitialiser {
 
+  /**
+   * Information about the addon that can be used prior to the addon
+   * being setup, along with the information that's required to create
+   * a new instance of the addon.
+   *
+   * @type {AddonInitialiser.Metadata}
+   * @memberof AddonInitialiser
+   */
   readonly metadata: AddonInitialiser.Metadata;
 
   /**
    * Create and return a new `Addon` instance.
+   *
+   * This method must be implemented by addon authors.
+   *
+   * The `configOptions` parameter will be an array of options that were provided
+   * by the `metadata.configOptions` property. If an option's `required` property
+   * is `true` it is guaranteed that that option will be in the array and will
+   * have passed type checking.
    *
    * @param {Addon.Metadata} metadata
    * @param {{ [id: string]: any; }} [configOptions]
@@ -18,20 +33,39 @@ interface AddonInitialiser {
 
 namespace AddonInitialiser {
   export interface Metadata {
+    /**
+     * The user-friendly display name for the addon.
+     *
+     * @type {string}
+     * @memberof Metadata
+     */
     readonly name: string;
 
+    /**
+     * A user-friendly description of the addon.
+     *
+     * @type {string}
+     * @memberof Metadata
+     */
     readonly description: string;
 
     /**
      * When this value is `true` it indicates to consequences that more than one instance
      * of the addon is supported. When this value is `false` the user will only be able to
-     * create one instance of this addon.
+     * create one instance of this addon. For example, an addon may support multiple accounts
+     * on the same platform, which would create multiple distinct instances of the addon.
      *
      * @type {boolean}
      * @memberof Metadata
      */
     readonly supportsMultipleInstances: boolean;
 
+    /**
+     * Configuration options that may be passed to the `createInstance` function.
+     *
+     * @type {Addon.ConfigOption[]}
+     * @memberof Metadata
+     */
     readonly configOptions?: Addon.ConfigOption[];
   }
 
