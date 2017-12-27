a5a8c733634add05b950ef35efd43092d3f90257
Joseph Duffy <josephduffy@me.com>
2017-12-27 00:16:56 +0000

    Adds further test for `Link`
    Renames `Link`'s `conditions` property to `conditionalLinks`

--- src/models/Link.ts
+++ src/models/Link.ts
@@ -13,7 +13,7 @@ export default class Link {
    * this link is evaluated each of the conditions will be check. If
    * the condition returns `true` the link will be evaluated.
    */
-  public readonly conditions: Array<[Condition, UserInput.Value[], Link]>;
+  public readonly conditionalLinks: Array<[Condition, UserInput.Value[], Link]>;
 
   /**
    * An array of actions that will be performed when this link is reached
@@ -22,9 +22,9 @@ export default class Link {
    */
   public readonly actions: Array<[Action, UserInput.Value[]]>;
 
-  constructor(id: string, conditions: Array<[Condition, UserInput.Value[], Link]>, actions: Array<[Action, UserInput.Value[]]>) {
+  constructor(id: string, conditionalLinks: Array<[Condition, UserInput.Value[], Link]>, actions: Array<[Action, UserInput.Value[]]>) {
     this.id = id;
-    this.conditions = conditions;
+    this.conditionalLinks = conditionalLinks;
     this.actions = actions;
   }
 
@@ -33,7 +33,7 @@ export default class Link {
       await action.perform(userInputs);
     }
 
-    for (const [condition, userInputs, link] of this.conditions) {
+    for (const [condition, userInputs, link] of this.conditionalLinks) {
       if (await condition.evaluate(userInputs)) {
         await link.evaluate();
       }
--- src/models/__tests__/Link.ts
+++ src/models/__tests__/Link.ts
@@ -1,8 +1,10 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import Action from '../../interfaces/Action';
+import Condition from '../../interfaces/Condition';
 import UserInput from '../../interfaces/UserInput';
 import Link from '../Link';
+import BooleanCondition from '../../conditionals/BooleanCondition';
 
 describe('Link', () => {
   describe('#evaluate()', () => {
@@ -26,6 +28,47 @@ describe('Link', () => {
 
       stubs.forEach(stub => {
         expect(stub.calledOnce).to.be.true;
+      });
+    });
+
+    it('should only evaluate conditional links that meet their requirements', async () => {
+      const conditionalLinks: Array<[Condition, UserInput.Value[], Link]> = [];
+      const stubsToBeCalled: sinon.SinonStub[] = [];
+      const stubsToNotBeCalled: sinon.SinonStub[] = [];
+
+      for (let i = 0; i < 10; i++) {
+        const condition = new BooleanCondition.True();
+        const link = new Link(`test-link-${i}`, [], []);
+        const stub = sinon.stub(link, 'evaluate');
+        const shouldSucceed = i % 2 === 0;
+
+        conditionalLinks.push([
+          condition,
+          [
+            {
+              uniqueId: 'input',
+              value: shouldSucceed,
+            },
+          ],
+          link,
+        ]);
+
+        if (shouldSucceed) {
+          stubsToBeCalled.push(stub);
+        } else {
+          stubsToNotBeCalled.push(stub);
+        }
+      }
+
+      const rootLink = new Link('root-test-link-1', conditionalLinks, []);
+      await rootLink.evaluate();
+
+      stubsToBeCalled.forEach(stub => {
+        expect(stub.calledOnce).to.be.true;
+      });
+
+      stubsToNotBeCalled.forEach(stub => {
+        expect(stub.notCalled).to.be.true;
       });
     });
   });
